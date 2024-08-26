15ca44e388ed9ee471ebab164bdd1aedca6a1b45
Chris_S <chris.s@apluskitchen.com>
2024-08-26 11:58:47 -0700

    setup sequelize and connected the schema to its db

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const { Sequelize, DataTypes } = require('sequelize');
+import Sequelize from 'sequelize'
+
+/* import userModel from './user'
+import messageModel from './message' */
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DBA_USER, process.env.DB_PASSWORD, {
+    dialect: 'postgres'
+})
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +20,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync( { force: false }).then ( () => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
